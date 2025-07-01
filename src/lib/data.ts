9a84955dc2004c870faed7658456e4351bb685e8
Mostafa <mostafasiyam29@gmail.com>
2025-07-01 22:10:54 +0600

    modified data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -9,15 +9,62 @@ export const teachersData = [
     teacherId: "1234567890",
     name: "DR. YOUNUS",
     email: "younus2025@gmail.com",
-   
+    photo:
+      "https://images.pexels.com/photos/936126/pexels-photo-936126.jpeg?auto=compress&cs=tinysrgb&w=1200",
     phone: "1234567890",
     subjects: ["Math"],
     classes: ["204"],
     address: "DHAKA, BANGLADESH",
   },
   
-  
+  {
+    "id": 2,
+    "teacherId": "1234567890",
+    "name": "Rahima Khatun",
+    "email": "rahima.khatun@school.com.bd",
+    "photo": "https://images.pexels.com/photos/936126/pexels-photo-936126.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01711223344",
+    "subjects": ["Physics", "Chemistry"],
+    "classes": ["5A", "4B", "3C"],
+    "address": "House 12, Road 5, Dhanmondi, Dhaka 1205, Bangladesh"
+  },
+  {
+    "id": 3,
+    "teacherId": "1234567890",
+    "name": "Md. Azizur Rahman",
+    "email": "azizur.rahman@school.com.bd",
+    "photo": "https://images.pexels.com/photos/428328/pexels-photo-428328.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01812345678",
+    "subjects": ["Biology"],
+    "classes": ["5A", "4B", "3C"],
+    "address": "Sector 10, Road 2, Uttara, Dhaka 1230, Bangladesh"
+  },
+  {
+    "id": 4,
+    "teacherId": "1234567890",
+    "name": "Zahirul Islam",
+    "email": "zahirul.islam@school.com.bd",
+    "photo": "https://images.pexels.com/photos/1187765/pexels-photo-1187765.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01678901234",
+    "subjects": ["History"],
+    "classes": ["5A", "4B", "3C"],
+    "address": "42 K.B. Road, Khulna 9100, Bangladesh"
+  },
+  {
+    "id": 5,
+    "teacherId": "1234567890",
+    "name": "Sharmin Akter",
+    "email": "sharmin.akter@school.com.bd",
+    "photo": "https://images.pexels.com/photos/1102341/pexels-photo-1102341.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01555556666",
+    "subjects": ["Music", "History"],
+    "classes": ["5A", "4B", "3C"],
+    "address": "Chandgaon Residential Area, Chattogram 4212, Bangladesh"
+  }
+
+
 ];
+
 
 export const studentsData = [
   
@@ -33,9 +80,184 @@ export const studentsData = [
     class: "420",
     address: "Deli, india",
   },
+  {
+    "id": 2,
+    "studentId": "1234567890",
+    "name": "Sumaiya Rahman",
+    "email": "sumaiya.rahman@student.com.bd",
+    "photo": "https://images.pexels.com/photos/936126/pexels-photo-936126.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01711112222",
+    "grade": 5,
+    "class": "5A",
+    "address": "Mirpur DOHS, Dhaka 1216, Bangladesh"
+  },
+  {
+    "id": 3,
+    "studentId": "1234567890",
+    "name": "Sajid Ahmed",
+    "email": "sajid.ahmed@student.com.bd",
+    "photo": "https://images.pexels.com/photos/428328/pexels-photo-428328.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01812345678",
+    "grade": 5,
+    "class": "5A",
+    "address": "Kazipara, Mirpur, Dhaka 1216, Bangladesh"
+  },
+  {
+    "id": 4,
+    "studentId": "1234567890",
+    "name": "Nazia Hasan",
+    "email": "nazia.hasan@student.com.bd",
+    "photo": "https://images.pexels.com/photos/1187765/pexels-photo-1187765.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01678901234",
+    "grade": 5,
+    "class": "5A",
+    "address": "Mohammadpur, Dhaka 1207, Bangladesh"
+  },
+  {
+    "id": 5,
+    "studentId": "1234567890",
+    "name": "Tanvir Alam",
+    "email": "tanvir.alam@student.com.bd",
+    "photo": "https://images.pexels.com/photos/1102341/pexels-photo-1102341.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01755667788",
+    "grade": 5,
+    "class": "5A",
+    "address": "Dhanmondi, Dhaka 1205, Bangladesh"
+  },
+  {
+    "id": 6,
+    "studentId": "1234567890",
+    "name": "Maliha Akter",
+    "email": "maliha.akter@student.com.bd",
+    "photo": "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01555556666",
+    "grade": 5,
+    "class": "5A",
+    "address": "Kotwali, Chattogram 4000, Bangladesh"
+  },
+  {
+    "id": 7,
+    "studentId": "1234567890",
+    "name": "Fahim Hossain",
+    "email": "fahim.hossain@student.com.bd",
+    "photo": "https://images.pexels.com/photos/1438081/pexels-photo-1438081.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01999887766",
+    "grade": 5,
+    "class": "5A",
+    "address": "Shibganj, Bogura 5800, Bangladesh"
+  },
+  {
+    "id": 8,
+    "studentId": "1234567890",
+    "name": "Mehjabin Chowdhury",
+    "email": "mehjabin.chowdhury@student.com.bd",
+    "photo": "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01300112233",
+    "grade": 5,
+    "class": "5A",
+    "address": "Halishahar, Chattogram 4216, Bangladesh"
+  },
+  {
+    "id": 9,
+    "studentId": "1234567890",
+    "name": "Rakin Islam",
+    "email": "rakin.islam@student.com.bd",
+    "photo": "https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01888889999",
+    "grade": 5,
+    "class": "5A",
+    "address": "Rajshahi University Area, Rajshahi 6205, Bangladesh"
+  },
+  {
+    "id": 10,
+    "studentId": "1234567890",
+    "name": "Tasmia Noor",
+    "email": "tasmia.noor@student.com.bd",
+    "photo": "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "phone": "01477443322",
+    "grade": 5,
+    "class": "5A",
+    "address": "Zindabazar, Sylhet 3100, Bangladesh"
+  }
+
+
 ];
 
 export const parentsData = [
+
+  {
+    "id": 2,
+    "name": "Rehana Begum",
+    "students": ["Sumaiya Rahman"],
+    "email": "rehana.begum@guardian.com.bd",
+    "phone": "01711223344",
+    "address": "Mirpur DOHS, Dhaka 1216, Bangladesh"
+  },
+  {
+    "id": 3,
+    "name": "Abdul Hannan",
+    "students": ["Sajid Ahmed"],
+    "email": "abdul.hannan@guardian.com.bd",
+    "phone": "01812345678",
+    "address": "Kazipara, Mirpur, Dhaka 1216, Bangladesh"
+  },
+  {
+    "id": 4,
+    "name": "Jahanara Khatun",
+    "students": ["Nazia Hasan", "Tanvir Alam"],
+    "email": "jahanara.khatun@guardian.com.bd",
+    "phone": "01678901234",
+    "address": "Mohammadpur, Dhaka 1207, Bangladesh"
+  },
+  {
+    "id": 5,
+    "name": "Nasir Uddin",
+    "students": ["Maliha Akter"],
+    "email": "nasir.uddin@guardian.com.bd",
+    "phone": "01755667788",
+    "address": "Dhanmondi, Dhaka 1205, Bangladesh"
+  },
+  {
+    "id": 6,
+    "name": "Roksana Akter",
+    "students": ["Fahim Hossain"],
+    "email": "roksana.akter@guardian.com.bd",
+    "phone": "01555556666",
+    "address": "Kotwali, Chattogram 4000, Bangladesh"
+  },
+  {
+    "id": 7,
+    "name": "Habibul Bashar",
+    "students": ["Mehjabin Chowdhury", "Rakin Islam"],
+    "email": "habibul.bashar@guardian.com.bd",
+    "phone": "01999887766",
+    "address": "Shibganj, Bogura 5800, Bangladesh"
+  },
+  {
+    "id": 8,
+    "name": "Nurun Nahar",
+    "students": ["Tasmia Noor"],
+    "email": "nurun.nahar@guardian.com.bd",
+    "phone": "01300112233",
+    "address": "Halishahar, Chattogram 4216, Bangladesh"
+  },
+  {
+    "id": 9,
+    "name": "Mokhlesur Rahman",
+    "students": ["Rakin Islam", "Mehjabin Chowdhury"],
+    "email": "mokhlesur.rahman@guardian.com.bd",
+    "phone": "01888889999",
+    "address": "Rajshahi University Area, Rajshahi 6205, Bangladesh"
+  },
+  {
+    "id": 10,
+    "name": "Salma Jahan",
+    "students": ["Tasmia Noor"],
+    "email": "salma.jahan@guardian.com.bd",
+    "phone": "01477443322",
+    "address": "Zindabazar, Sylhet 3100, Bangladesh"
+  }
+
   
 ];
 
@@ -74,30 +296,539 @@ export const subjectsData = [
 ];
 
 export const classesData = [
+
+  {
+    "id": 1,
+    "name": "1A",
+    "capacity": 20,
+    "grade": 1,
+    "supervisor": "Abdul Karim"
+  },
+  {
+    "id": 2,
+    "name": "2B",
+    "capacity": 22,
+    "grade": 2,
+    "supervisor": "Farzana Akter"
+  },
+  {
+    "id": 3,
+    "name": "3C",
+    "capacity": 20,
+    "grade": 3,
+    "supervisor": "Shamsul Alam"
+  },
+  {
+    "id": 4,
+    "name": "4B",
+    "capacity": 18,
+    "grade": 4,
+    "supervisor": "Rokeya Sultana"
+  },
+  {
+    "id": 5,
+    "name": "5A",
+    "capacity": 16,
+    "grade": 5,
+    "supervisor": "Nasima Haque"
+  },
+  {
+    "id": 5,
+    "name": "5B",
+    "capacity": 20,
+    "grade": 5,
+    "supervisor": "Shahidul Islam"
+  },
+  {
+    "id": 7,
+    "name": "7A",
+    "capacity": 18,
+    "grade": 7,
+    "supervisor": "Sharmin Jahan"
+  },
+  {
+    "id": 8,
+    "name": "6B",
+    "capacity": 22,
+    "grade": 6,
+    "supervisor": "Mahmudul Hasan"
+  },
+  {
+    "id": 9,
+    "name": "6C",
+    "capacity": 18,
+    "grade": 6,
+    "supervisor": "Rashed Chowdhury"
+  },
+  {
+    "id": 10,
+    "name": "6D",
+    "capacity": 20,
+    "grade": 6,
+    "supervisor": "Sanjida Alam"
+  }
+
   
 ];
 
 export const lessonsData = [
+  
+  {
+    "id": 1,
+    "subject": "Math",
+    "class": "1A",
+    "teacher": "Abdul Matin"
+  },
+  {
+    "id": 2,
+    "subject": "English",
+    "class": "2A",
+    "teacher": "Salma Akter"
+  },
+  {
+    "id": 3,
+    "subject": "Science",
+    "class": "3A",
+    "teacher": "Tariqul Islam"
+  },
+  {
+    "id": 4,
+    "subject": "Social Studies",
+    "class": "1B",
+    "teacher": "Shamima Nasrin"
+  },
+  {
+    "id": 5,
+    "subject": "Art",
+    "class": "4A",
+    "teacher": "Jamal Uddin"
+  },
+  {
+    "id": 6,
+    "subject": "Music",
+    "class": "5A",
+    "teacher": "Sharmin Sultana"
+  },
+  {
+    "id": 7,
+    "subject": "History",
+    "class": "6A",
+    "teacher": "Nazmul Huda"
+  },
+  {
+    "id": 8,
+    "subject": "Geography",
+    "class": "6B",
+    "teacher": "Munira Ahmed"
+  },
+  {
+    "id": 9,
+    "subject": "Physics",
+    "class": "6C",
+    "teacher": "Rafiqul Islam"
+  },
+  {
+    "id": 10,
+    "subject": "Chemistry",
+    "class": "4B",
+    "teacher": "Tahmina Chowdhury"
+  }
+
  
 ];
 
 export const examsData = [
   
+  {
+    "id": 1,
+    "subject": "Math",
+    "class": "1A",
+    "teacher": "Abdur Rahman"
+  },
+  {
+    "id": 2,
+    "subject": "English",
+    "class": "2A",
+    "teacher": "Farzana Haque"
+  },
+  {
+    "id": 3,
+    "subject": "Science",
+    "class": "3A",
+    "teacher": "Rafiqul Islam"
+  },
+  {
+    "id": 4,
+    "subject": "Social Studies",
+    "class": "1B",
+    "teacher": "Shamima Akter"
+  },
+  {
+    "id": 5,
+    "subject": "Art",
+    "class": "4A",
+    "teacher": "Jamal Hossain"
+  },
+  {
+    "id": 6,
+    "subject": "Music",
+    "class": "5A",
+    "teacher": "Sharmin Sultana"
+  },
+  {
+    "id": 7,
+    "subject": "History",
+    "class": "6A",
+    "teacher": "Nazmul Karim"
+  },
+  {
+    "id": 8,
+    "subject": "Geography",
+    "class": "6B",
+    "teacher": "Munira Chowdhury"
+  },
+  {
+    "id": 9,
+    "subject": "Physics",
+    "class": "6C",
+    "teacher": "Mahmudul Hasan"
+  },
+  {
+    "id": 10,
+    "subject": "Chemistry",
+    "class": "4B",
+    "teacher": "Tahmina Rahman"
+  }
+
+  
 ];
 
 export const assignmentsData = [
   
+  {
+    "id": 1,
+    "subject": "Math",
+    "class": "1A",
+    "teacher": "Abdul Karim",
+    "dueDate": "2025-06-27"
+  },
+  {
+    "id": 2,
+    "subject": "English",
+    "class": "2A",
+    "teacher": "Farzana Yasmin",
+    "dueDate": "2025-06-27"
+  },
+  {
+    "id": 3,
+    "subject": "Science",
+    "class": "3A",
+    "teacher": "Rafiqul Alam",
+    "dueDate": "2025-06-27"
+  },
+  {
+    "id": 4,
+    "subject": "Social Studies",
+    "class": "1B",
+    "teacher": "Shamima Akhter",
+    "dueDate": "2025-06-27"
+  },
+  {
+    "id": 5,
+    "subject": "Art",
+    "class": "4A",
+    "teacher": "Jamal Hossain",
+    "dueDate": "2025-06-27"
+  },
+  {
+    "id": 6,
+    "subject": "Music",
+    "class": "5A",
+    "teacher": "Sharmin Sultana",
+    "dueDate": "2025-06-27"
+  },
+  {
+    "id": 7,
+    "subject": "History",
+    "class": "6A",
+    "teacher": "Nazmul Huda",
+    "dueDate": "2025-06-27"
+  },
+  {
+    "id": 8,
+    "subject": "Geography",
+    "class": "6B",
+    "teacher": "Munira Ahmed",
+    "dueDate": "2025-06-27"
+  },
+  {
+    "id": 9,
+    "subject": "Physics",
+    "class": "7A",
+    "teacher": "Mahmudul Hasan",
+    "dueDate": "2025-06-27"
+  },
+  {
+    "id": 10,
+    "subject": "Chemistry",
+    "class": "8A",
+    "teacher": "Tahmina Rahman",
+    "dueDate": "2025-06-27"
+  }
+  
 ];
 
 export const resultsData = [
-  
+
+  {
+    "id": 1,
+    "subject": "Math",
+    "class": "1A",
+    "teacher": "Abdul Karim",
+    "student": "Rafiq Ahmed",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  },
+  {
+    "id": 2,
+    "subject": "English",
+    "class": "2A",
+    "teacher": "Farzana Haque",
+    "student": "Nabila Akter",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  },
+  {
+    "id": 3,
+    "subject": "Science",
+    "class": "3A",
+    "teacher": "Rafiqul Islam",
+    "student": "Hasan Mahmud",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  },
+  {
+    "id": 4,
+    "subject": "Social Studies",
+    "class": "1B",
+    "teacher": "Shamima Akter",
+    "student": "Sadia Rahman",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  },
+  {
+    "id": 5,
+    "subject": "Art",
+    "class": "4A",
+    "teacher": "Jamal Hossain",
+    "student": "Mintu Hossain",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  },
+  {
+    "id": 6,
+    "subject": "Music",
+    "class": "5A",
+    "teacher": "Sharmin Sultana",
+    "student": "Tahsin Chowdhury",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  },
+  {
+    "id": 7,
+    "subject": "History",
+    "class": "6A",
+    "teacher": "Nazmul Karim",
+    "student": "Sumaiya Nasrin",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  },
+  {
+    "id": 8,
+    "subject": "Geography",
+    "class": "6B",
+    "teacher": "Munira Chowdhury",
+    "student": "Imran Hossain",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  },
+  {
+    "id": 9,
+    "subject": "Physics",
+    "class": "7A",
+    "teacher": "Mahmudul Hasan",
+    "student": "Rana Biswas",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  },
+  {
+    "id": 10,
+    "subject": "Chemistry",
+    "class": "8A",
+    "teacher": "Tahmina Rahman",
+    "student": "Parvez Ahmed",
+    "date": "2025-01-01",
+    "type": "exam",
+    "score": 90
+  } 
 ];
 
 export const eventsData = [
-  
+
+  {
+    "id": 1,
+    "title": "Padma River Excursion",
+    "class": "1A",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  },
+  {
+    "id": 2,
+    "title": "Village Picnic",
+    "class": "2A",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  },
+  {
+    "id": 3,
+    "title": "Cox’s Bazar Beach Trip",
+    "class": "3A",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  },
+  {
+    "id": 4,
+    "title": "Bangladesh National Museum Visit",
+    "class": "4A",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  },
+  {
+    "id": 5,
+    "title": "Traditional Music Concert",
+    "class": "5A",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  },
+  {
+    "id": 6,
+    "title": "Magic Show by Fakir",
+    "class": "1B",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  },
+  {
+    "id": 7,
+    "title": "Sundarbans Boat Trip",
+    "class": "2B",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  },
+  {
+    "id": 8,
+    "title": "Cycle Rally",
+    "class": "3B",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  },
+  {
+    "id": 9,
+    "title": "Local Art Exhibition",
+    "class": "4B",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  },
+  {
+    "id": 10,
+    "title": "Inter-school Football Tournament",
+    "class": "5B",
+    "date": "2025-06-30",
+    "startTime": "10:00",
+    "endTime": "11:00"
+  }
+
 ];
 
 export const announcementsData = [
+  
+  {
+    id: 1,
+    title: "Notice for Class 4A Math Exam",
+    class: "4A",
+    date: "2025-07-01",
+  },
+  {
+    id: 2,
+    title: "Notice for Class 3A Math Exam",
+    class: "3A",
+    date: "2025-07-01",
+  },
+  {
+    id: 3,
+    title: "Notice for Class 3B Math Exam",
+    class: "3B",
+    date: "2025-07-01",
+  },
+  {
+    id: 4,
+    title: "Notice for Class 6A Math Exam",
+    class: "6A",
+    date: "2025-07-01",
+  },
+  {
+    id: 5,
+    title: "Notice for Class 8C Math Exam",
+    class: "8C",
+    date: "2025-07-01",
+  },
+  {
+    id: 6,
+    title: "Notice for Class 2A Math Exam",
+    class: "2A",
+    date: "2025-07-01",
+  },
+  {
+    id: 7,
+    title: "Notice for Class 4C Math Exam",
+    class: "4C",
+    date: "2025-07-01",
+  },
+  {
+    id: 8,
+    title: "Notice for Class 4B Math Exam",
+    class: "4B",
+    date: "2025-07-01",
+  },
+  {
+    id: 9,
+    title: "Notice for Class 3C Math Exam",
+    class: "3C",
+    date: "2025-07-01",
+  },
+  {
+    id: 10,
+    title: "Notice for Class 1C Math Exam",
+    class: "1C",
+    date: "2025-07-01",
+  },
   
 ];
 
